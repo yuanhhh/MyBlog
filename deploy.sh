20d1522cdf3ff4ade15d450c98583d5078acacc1
yuanhui <yuanhui@gitlab.aifound.cn>
2020-08-21 11:51:16 +0800

    修改shell

--- deploy.sh
+++ deploy.sh
@@ -12,14 +12,17 @@ cd docs/.vuepress/dist
 # 如果是发布到自己定义的域名
 # echo 'www.mysite.com' >CNAME
 
-git intit
+git init
 
-git add -A
+git add .
 
 git commit -m 'deploy'
 
+# 链接远程仓库
+#git remote add origin https://github.com/yuanhhh/yuanhhh.github.io-.git
+
 # 如果你想要部署到 https://USERNAME.github.io
-git push -f git@github.com:yuanhhh/yuanhhh.github.io.git master
+git push -f https://github.com/yuanhhh/yuanhhh.github.io-.git master
 
 # 如果发布到 https://USERNAME.github.io/<REPO>  REPO=github上的项目
 # git push -f git@github.com:USERNAME/<REPO>.git master:gh-pages
